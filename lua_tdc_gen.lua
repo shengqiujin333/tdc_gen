745c20acc3f064d9a40382295421bcd91c93466f
shengqiujin <shengqiujin333@gmail.com>
2022-03-02 10:23:12 +0800

    bug fix for lua_tdc_gen

--- lua_tdc_gen.lua
+++ lua_tdc_gen.lua
@@ -41,7 +41,7 @@ for i, line in ftcsv.parseLine("canshu.csv", ",", {bufferSize=50}) do
     tdc1000_conf[1],tdc1000_conf[2],tdc1000_conf[3],
     tdc1000_conf[4],tdc1000_conf[5],tdc1000_conf[6],
     tdc1000_conf[7],tdc1000_conf[8],tdc1000_conf[9],tdc1000_conf[10] = tdc.tdc1000_gen(xlen,xtfreq,xtvol,xcfreq)
-
+    --print(tdc1000_conf[5])
     tdc7200_conf[1],tdc7200_conf[2],tdc7200_conf[3],
     tdc7200_conf[4],tdc7200_conf[5],tdc7200_conf[6],
     tdc7200_conf[7],tdc7200_conf[8],tdc7200_conf[9],tdc7200_conf[10] = tdc.tdc7200_gen()
@@ -49,38 +49,38 @@ for i, line in ftcsv.parseLine("canshu.csv", ",", {bufferSize=50}) do
     temp_conf[1],temp_conf[2],temp_conf[3],
     temp_conf[4],temp_conf[5],temp_conf[6],temp_conf[7] = tdc.tdc_by_temperature(temp_max,xcfreq,xlen,xtfreq,17)
 
-    tdc1000_conf[5] = tdc1000_conf[5] + temp_conf[1]
-    tdc1000_conf[6] = tdc1000_conf[6] + temp_conf[2]
-    tdc1000_conf[9] = tdc1000_conf[9] + temp_conf[3]
+    tdc1000_conf[6] = tdc1000_conf[6] + temp_conf[1]
+    tdc1000_conf[7] = tdc1000_conf[7] + temp_conf[2]
+    tdc1000_conf[10] = tdc1000_conf[10] + temp_conf[3]
 
-    tdc7200_conf[6] = tdc7200_conf[6] + temp_conf[4]
-    tdc7200_conf[7] = tdc7200_conf[7] + temp_conf[5]
-    tdc7200_conf[8] = tdc7200_conf[8] + temp_conf[6]
-    tdc7200_conf[9] = tdc7200_conf[9] + temp_conf[7]
+    tdc7200_conf[7] = tdc7200_conf[7] + temp_conf[4]
+    tdc7200_conf[8] = tdc7200_conf[8] + temp_conf[5]
+    tdc7200_conf[9] = tdc7200_conf[9] + temp_conf[6]
+    tdc7200_conf[10] = tdc7200_conf[10] + temp_conf[7]
 
     data[i] = {}
     data[i].size = xsize
-    data[i].config0 = string.format("%x",tdc1000_conf[1])
-    data[i].config1 = string.format("%x",tdc1000_conf[2])
-    data[i].config2 = string.format("%x",tdc1000_conf[3])
-    data[i].config3 = string.format("%x",tdc1000_conf[4])
-    data[i].config4 = string.format("%x",tdc1000_conf[5])
-    data[i].tof1 = string.format("%x",tdc1000_conf[6])
-    data[i].tof0 = string.format("%x",tdc1000_conf[7])
-    data[i].error_flags = string.format("%x",tdc1000_conf[8])
-    data[i].timeout = string.format("%x",tdc1000_conf[9])
-    data[i].clock_rate = string.format("%x",tdc1000_conf[10])
+    data[i].tdc1000_config0 = string.format("%x",tdc1000_conf[1])
+    data[i].tdc1000_config1 = string.format("%x",tdc1000_conf[2])
+    data[i].tdc1000_config2 = string.format("%x",tdc1000_conf[3])
+    data[i].tdc1000_config3 = string.format("%x",tdc1000_conf[4])
+    data[i].tdc1000_config4 = string.format("%x",tdc1000_conf[5])
+    data[i].tdc1000_tof1 = string.format("%x",tdc1000_conf[6])
+    data[i].tdc1000_tof0 = string.format("%x",tdc1000_conf[7])
+    data[i].tdc1000_error_flags = string.format("%x",tdc1000_conf[8])
+    data[i].tdc1000_timeout = string.format("%x",tdc1000_conf[9])
+    data[i].tdc1000_clock_rate = string.format("%x",tdc1000_conf[10])
 
-    data[i].config1 = string.format("%x",tdc7200_conf[1])
-    data[i].config2 = string.format("%x",tdc7200_conf[2])
-    data[i].int_status = string.format("%x",tdc7200_conf[3])
-    data[i].int_mask  = string.format("%x",tdc7200_conf[4])
-    data[i].coarse_h = string.format("%x",tdc7200_conf[5])
-    data[i].coarse_l = string.format("%x",tdc7200_conf[6])
-    data[i].clock_ovf_h = string.format("%x",tdc7200_conf[7])
-    data[i].clock_ovf_l = string.format("%x",tdc7200_conf[8])
-    data[i].clock_mask_h = string.format("%x",tdc7200_conf[9])
-    data[i].clock_mask_l = string.format("%x",tdc7200_conf[10])
+    data[i].tdc7200_config1 = string.format("%x",tdc7200_conf[1])
+    data[i].tdc7200_config2 = string.format("%x",tdc7200_conf[2])
+    data[i].tdc7200_int_status = string.format("%x",tdc7200_conf[3])
+    data[i].tdc7200_int_mask  = string.format("%x",tdc7200_conf[4])
+    data[i].tdc7200_coarse_h = string.format("%x",tdc7200_conf[5])
+    data[i].tdc7200_coarse_l = string.format("%x",tdc7200_conf[6])
+    data[i].tdc7200_clock_ovf_h = string.format("%x",tdc7200_conf[7])
+    data[i].tdc7200_clock_ovf_l = string.format("%x",tdc7200_conf[8])
+    data[i].tdc7200_clock_mask_h = string.format("%x",tdc7200_conf[9])
+    data[i].tdc7200_clock_mask_l = string.format("%x",tdc7200_conf[10])
 
     --break
 end
